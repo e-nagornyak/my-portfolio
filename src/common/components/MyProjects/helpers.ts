60f8879f3cd8a3bed14334dc8b51d2c14e79d13a
Evhen Nagornyak <dolkiv@gmail.com>
2023-07-11 15:24:28 +0300

    added projects

---
+++ src/common/components/MyProjects/helpers.ts
@@ -0,0 +1,29 @@
+
+import todoImage from "assets/image/todolist.png";
+import grandschool from "assets/image/grandschool.png";
+import cards from "assets/image/cards.jpg";
+import navus from "assets/image/navus.png";
+
+export const PROJECTS = [
+    {
+        title: 'grandschool',
+        styles: grandschool,
+        href: 'https://grandschool.pl/'
+    },
+    {
+        title: 'cards',
+        styles: cards,
+        href: 'https://e-nagornyak.github.io/cards/'
+    },
+    {
+        title: 'todolist',
+        styles: todoImage,
+        href: 'https://e-nagornyak.github.io/task-manager'
+    },  {
+        title: 'navus',
+        styles: navus,
+        href: 'https://navus-venture.vercel.app/'
+    },
+
+
+]
